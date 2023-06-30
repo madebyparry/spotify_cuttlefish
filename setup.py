ad0e636dbf6d181129762ad1e0295dcf885cf255
madebyparry <nicholas@madebyparry.com>
2023-06-30 15:11:21 -0400

    Beginning setup utils

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+try:
+    # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:
+    # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+def load_requirements(fname):
+    reqs = parse_requirements(fname, session="test")
+    return [str(ir.req) for ir in reqs]
+
+setup(name="spotify_cuttlefish", install_requires=load_requirements("requirements.txt"))
